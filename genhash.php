faef389507af9d4b68c8e5f0be67accfc7076599
Aryel Tupinambá <aryel.tupinamba@lqdi.net>
2016-08-22 14:41:02 -0300

    Added password max length to avoid server CPU exploiting

--- genhash.php
+++ genhash.php
@@ -1,4 +1,9 @@
 <?php
+
+if(strlen($_GET['password']) > 256) {
+	die(json_encode(['status' => 'password_too_large']));
+}
+
 echo json_encode([
 	'password' => $_GET['password'],
 	'bcrypt' => password_hash($_GET['password'], PASSWORD_BCRYPT),
